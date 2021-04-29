7c17ebbbca3e74db4364a55dc998b932038da8e1
Patrick <zhen.patrick@gmail.com>
2021-04-29 12:09:41 -0400

    styled home name

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,8 +5,13 @@ const Navbar = () => {
   return (
     <header className="bg-red-600">
       <div className="container mx-auto flex justify-between">
-        <nav>
-          <NavLink to="/" exact>
+        <nav className="flex">
+          <NavLink
+            to="/"
+            exact
+            activeClassName="text-white"
+            className="inflex-flex, items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest"
+          >
             Patrick
           </NavLink>
           <NavLink to="/BlogPost">Blog Posts</NavLink>
