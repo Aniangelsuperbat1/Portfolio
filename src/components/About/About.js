4c98a74c0c4fca109e6e8ce62a2f3738c7fba832
Patrick <zhen.patrick@gmail.com>
2021-05-03 20:53:29 -0400

    changed text to white to show more

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -30,7 +30,7 @@ const About = () => {
     <main className="relative">
       <img src="" alt="" className="absolute w-full" />
       <div className="p-10 lg:pt-48 container mx-auto relative">
-        <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
+        <section className="bg-green-800 text-white rounded-lg shadow-2xl lg:flex p-20">
           <img
             src={urlFor(about.authorImage).url()}
             alt=""
@@ -42,7 +42,7 @@ const About = () => {
               Hello. I am{" "}
               <span className="text-green-100">{about.name}</span>
             </h1>
-            <div className="prose lg:prose-xl text-green">
+            <div className="prose lg:prose-xl">
               <BlockContent
                 blocks={about.bio}
                 projectId="9l1mb7ik"
