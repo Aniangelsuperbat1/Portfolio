24eb4d018bdec2a8bceb9b16b4f925b0955c5c95
Patrick <zhen.patrick@gmail.com>
2021-05-05 19:57:35 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from "./logo.svg";
-import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
