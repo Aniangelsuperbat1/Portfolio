dd8bf86c5587bac186442204776f6667528bf1c8
Patrick <zhen.patrick@gmail.com>
2021-05-06 14:21:21 -0400

    changed about me page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import sanityClient from "../../connect.js";
-import imageUrlBuilder from "@sanity/image-url"
-import BlockContent from "@sanity/block-content-to-react"
+import imageUrlBuilder from "@sanity/image-url";
+import BlockContent from "@sanity/block-content-to-react";
 
 const About = () => {
   const [about, setabout] = useState(null);
@@ -22,15 +22,15 @@ const About = () => {
       .catch(console.err);
   }, []);
 
-  if(!about){
-      return <div>Loading...</div>
+  if (!about) {
+    return <div>Loading...</div>;
   }
 
   return (
     <main className="relative">
-      <img src="" alt="" className="absolute w-full" />
+      {/* <img src={image} alt="" className="absolute w-full" /> */}
       <div className="p-10 lg:pt-48 container mx-auto relative">
-        <section className="bg-green-800 text-white rounded-lg shadow-2xl lg:flex p-20">
+        <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
           <img
             src={urlFor(about.authorImage).url()}
             alt=""
@@ -38,11 +38,10 @@ const About = () => {
             alt={about.name}
           />
           <div className="text-lg flex flex-col justify-center">
-            <h1 className="sursive text-6xl text-green-300 mb-4">
-              Hello. I am{" "}
-              <span className="text-green-100">{about.name}</span>
+            <h1 className="cursive text-6xl text-green-50 mb-4">
+              Hello. I am <span className="text-green-50">{about.name}</span>
             </h1>
-            <div className="prose lg:prose-xl">
+            <div className="text-green-50">
               <BlockContent
                 blocks={about.bio}
                 projectId="9l1mb7ik"
